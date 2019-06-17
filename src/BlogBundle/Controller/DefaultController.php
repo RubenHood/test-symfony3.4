228027aaba5d871c5082887059a33e0bbf7bbda3
Ruben__hood <rubenconb88@gmail.com>
2019-06-17 11:07:54 +0200

    configurando un bundle

---
+++ src/BlogBundle/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        //cambiar esto que viene por default
+        return $this->render('@Blog/Default/index.html.twig');
+    }
+}
