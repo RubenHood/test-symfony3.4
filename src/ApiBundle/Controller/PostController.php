3fc7ef620ff16d3f71f1371cb79047d9d4530a15
Ruben__hood <rubenconb88@gmail.com>
2019-06-26 13:28:21 +0200

    Setup an Api Rest

---
+++ src/ApiBundle/Controller/PostController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ApiBundle\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use BlogBundle\Entity\Post;
+
+/**
+ * @Route("/post")
+ */
+
+class PostController extends Controller
+{
+    
+}
