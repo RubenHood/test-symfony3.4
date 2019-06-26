2d06849992f6c3668c8e8dbd82acdf589c712917
Ruben__hood <rubenconb88@gmail.com>
2019-06-26 15:53:41 +0200

    Setup first EndPoint, Get All

--- src/ApiBundle/Controller/PostController.php
+++ src/ApiBundle/Controller/PostController.php
@@ -2,9 +2,14 @@
 
 namespace ApiBundle\Controller;
 
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
 use BlogBundle\Entity\Post;
 
 /**
@@ -13,5 +18,35 @@ use BlogBundle\Entity\Post;
 
 class PostController extends Controller
 {
+    public function serializePost(Post $post) {
+        return array(
+            'title' => $post->getTitle(),
+            'body' => $post->getBody(),
+            'user' => $post->getUser(),
+            'tag' => $post->getTag(),
+            'createAt' => $post->getCreateAt()
+        );
+    }
     
+    /**
+     * @Route("/getall", methods={"GET"})
+     */
+    public function getAllPostsAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $repository = $em->getRepository(Post::class);
+
+        $posts = $repository->findAll();
+
+        $data = array('post' => array());
+
+        foreach ($posts as $post) {
+            $data['post'][] = $this->serializePost($post);
+        }
+
+        $response = new JsonResponse($data, 200);
+        
+        return $response;
+    }
 }
